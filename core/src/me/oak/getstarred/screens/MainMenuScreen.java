418a0932f6c951f2b59448b6c0f8c04560a7f99a
White-Oak <lpzhelud@gmail.com>
2015-10-13 16:26:07 +0400

    Improved finding match screen

--- core/src/me/oak/getstarred/screens/MainMenuScreen.java
+++ core/src/me/oak/getstarred/screens/MainMenuScreen.java
@@ -7,6 +7,7 @@ import me.oak.getstarred.DebugActor;
 import me.oak.getstarred.network.Network;
 import me.oak.getstarred.network.messages.FindMessage;
 import spaceisnear.game.ui.Button;
+import spaceisnear.game.ui.Label;
 import spaceisnear.starting.ui.ScreenImprovedGreatly;
 
 /**
@@ -14,21 +15,28 @@ import spaceisnear.starting.ui.ScreenImprovedGreatly;
  * @author White Oak
  */
 @RequiredArgsConstructor public class MainMenuScreen extends ScreenImprovedGreatly {
-    
+
     private final Network network;
-    
+
     @Override
     public void create() {
 	setBackgroundColor(new Color(0x95a5a6ff));
 	getStage().addActor(new DebugActor());
-	Button reg = new Button("Find a game");
-	Button log = new Button("Quit a game");
-	reg.setPosition(getStage().getWidth() / 2 - (log.getWidth() + reg.getWidth() + 10) / 2,
-		getStage().getHeight() / 2 - reg.getHeight() / 2);
-	log.setPosition(reg.getX() + reg.getWidth() + 10, reg.getY());
-	getStage().addActor(log);
-	getStage().addActor(reg);
-	reg.setActivationListener(actor -> network.queue(new FindMessage(network.getDigest())));
-	log.setActivationListener(actor -> Gdx.app.exit());
+	final Button find = new Button("Find a game");
+	final Button quit = new Button("Quit a game");
+	final float halfWidth = getStage().getWidth() / 2;
+	final float halfHeight = getStage().getHeight() / 2;
+	find.setPosition(halfWidth - (find.getWidth()) / 2, halfHeight - 50);
+	quit.setPosition(halfWidth - (quit.getWidth()) / 2, halfHeight + 50);
+	getStage().addActor(quit);
+	getStage().addActor(find);
+	find.setActivationListener(actor -> {
+	    network.queue(new FindMessage(network.getDigest()));
+	    find.remove();
+	    Label label = new Label("Looking for a sign...");
+	    label.setPosition(halfWidth - label.getWidth() / 2, halfHeight - 30);
+	    getStage().addActor(label);
+	});
+	quit.setActivationListener(actor -> Gdx.app.exit());
     }
 }
