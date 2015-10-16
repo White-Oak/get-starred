7070264743799765b31c49128a31a73e4cf4060a
White-Oak <lpzhelud@gmail.com>
2015-10-16 16:06:32 +0400

    Changed display mode to fullscreen on desktop

--- desktop/src/me/oak/getstarred/desktop/DesktopLauncher.java
+++ desktop/src/me/oak/getstarred/desktop/DesktopLauncher.java
@@ -8,8 +8,9 @@ public class DesktopLauncher {
 
     public static void main(String[] arg) {
 	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-	config.width = 1280;
-	config.height = 720;
+	config.fullscreen = true;
+	config.width = 1920;
+	config.height = 1080;
 	new LwjglApplication(new MeltingPoint(), config);
     }
 }
