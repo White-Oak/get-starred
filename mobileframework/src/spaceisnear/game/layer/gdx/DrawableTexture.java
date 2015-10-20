9225323e8d1f70f513924337e626fa580907e5b7
White-Oak <lpzhelud@gmail.com>
2015-10-20 19:25:14 +0400

    Fixed crashing on ArrayOutOfBoundsException when drawing tiles

--- mobileframework/src/spaceisnear/game/layer/gdx/DrawableTexture.java
+++ mobileframework/src/spaceisnear/game/layer/gdx/DrawableTexture.java
@@ -1,6 +1,7 @@
 package spaceisnear.game.layer.gdx;
 
-import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import spaceisnear.game.layer.Contextable;
@@ -12,6 +13,9 @@ import spaceisnear.game.layer.Drawable;
 
     @Override
     public void paintTile(Contextable c, int x, int y, int id) {
+	if (id < 0 || id >= regions.length) {
+	    return;
+	}
 	ContextableTexture ct = (ContextableTexture) c;
 	Batch batch = ct.getBatch();
 	batch.draw(regions[id], x, y);
